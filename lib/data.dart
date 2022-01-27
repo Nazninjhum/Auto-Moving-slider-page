072ec491ec4157b8bd7b28c830ab863d6773e24c
Nazninjhum <nazninnaznin725@gmail.com>
2022-01-27 22:49:33 +0600

    auto_scroll App

---
+++ lib/data.dart
@@ -0,0 +1,34 @@
+List movies1 = [
+  'dja.jpg',
+  'drive.jpg',
+  'firsy.jpg',
+  'quite.jpg',
+  'seven.jpg',
+  'ter.jpg',
+];
+
+List movies2 = [
+  'baby.jpg',
+  'bas.jpg',
+  'joker.jpg',
+  'mad.jpg',
+  'once.jpg',
+  'warz.jpg',
+];
+
+List movies3 = [
+  'rocky.jpg',
+  'no.jpg',
+  'logan.jpg',
+  'fight.jpeg',
+  'godfather.jpg',
+  'leon.jpg',
+];
+List movies4 = [
+  'fight.jpeg',
+  'joker.jpg',
+  'firsy.jpg',
+  'godfather.jpg',
+  'seven.jpg',
+  'warz.jpg',
+];
